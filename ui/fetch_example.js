3401827b66beb1ecda8c01da4161b64d7b892745
sumitchauhan02 <46373576+sumitchauhan02@users.noreply.github.com>
2019-01-04 17:04:24 +0530

    Update fetch_example.js

--- ui/fetch_example.js
+++ ui/fetch_example.js
@@ -1,1 +1,13 @@
 
+<html>
+<head>
+    <title>Fetch API Demo</title>
+    <link rel="stylesheet" href="styles/fetch_style.css">
+</head>
+<body>
+    <input type="text" placeholder="Enter Subreddit e.g. javascript" />
+    <button>Fetch Posts</button>
+    <div id="result"></div>
+    <script src="js/fetch_example.js"></script>
+</body>
+</html>
