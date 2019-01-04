fa608f554453d9c88402f24db165172813f9fad2
sumitchauhan02 <46373576+sumitchauhan02@users.noreply.github.com>
2019-01-04 17:09:29 +0530

    Update webpack.config.js

--- ui/valentinog-redux/webpack.config.js
+++ ui/valentinog-redux/webpack.config.js
@@ -1,1 +1,40 @@
 
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+var path = require('path');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  devtool:"source-map",
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
